53787881bec0e9c3a80dd3350fb440e790123f83
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-06-26 17:44:47 +0200

    dzfhb

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener("'click", function() {
+    mobileMenu.classList.toggle('show-mobile-menu')
+
+    
+})
