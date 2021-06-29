2005541d4836f62dd18b9e6a363f473d7585ef6e
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-06-29 19:00:39 +0200

    aerg

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
+const hero = document.querySelector(".hero");
 
 hamburger.addEventListener("click", function () {
   mobileMenu.classList.toggle("show-mobile-menu");
+  hero.classList.toggle('reduce-hero-height')
 });
 
