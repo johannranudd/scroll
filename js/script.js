d47bd5ed8a384bd9cb5d1c5c438b5e068c5a3bba
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-06-30 16:43:16 +0200

    BEFORE I REDO

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
 const hero = document.querySelector(".hero");
+const navBar = document.querySelector(".nav-bar");
 
 hamburger.addEventListener("click", function () {
   mobileMenu.classList.toggle("show-mobile-menu");
   hero.classList.toggle('reduce-hero-height')
 });
 
+window.addEventListener("scroll", function () {
+  // console.log(scrollY);
+  if (scrollY > 90) {
+    navBar.classList.add('fixed-bar');
+    mobileMenu.classList.add('scroll-mobile-menu');
+  }
+  else {
+    navBar.classList.remove('fixed-bar');
+  }
+})
